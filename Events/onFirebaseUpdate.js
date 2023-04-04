3a4ad6eb153dd6fdbe0c24629126418d411b48f8
Emre Cebeci <emreyigitcbc@gmail.com>
2023-04-04 20:48:49 +0300

    Delete onFirebaseUpdate.js

--- Events/onFirebaseUpdate.js
+++
@@ -1,9 +0,0 @@
-global.db.ref("/").on("value", async function (snap) {
-  if(!snap.exists()) await global.db.ref("/").set(require("../default_config.json"));
-  global.db.cache = Object.assign(require("../default_config.json"), snap.val())
-  console.log("Database cache changed.")
-})
-
-module.exports = {
-	name: "onFirebaseRTDBUpdate"
-}
